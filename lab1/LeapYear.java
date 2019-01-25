43b05d85c35139c2edd5292392044bc171e2797d
Seungri Kim <srkim@berkeley.edu>
2019-01-25 14:53:38 -0800

    final work

--- lab1/LeapYear.java
+++ lab1/LeapYear.java
@@ -29,5 +29,11 @@ public class LeapYear {
             }
         }
     }
+    /** Check leapyear */
+    public static boolean isLeapYear(int year) {
+        if ((year % 400 == 0) || ((year % 4 == 0) && (year % 100 != 0))){
+          return true;
+      }
+        return false;
+    }
 }
-
