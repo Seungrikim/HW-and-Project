472448a829dfddfaea8fce4aab5cbbee26d7f996
Seungri Kim <srkim@berkeley.edu>
2019-02-06 17:50:38 -0800

    first

--- lab3/Flik/HorribleSteve.java
+++ lab3/Flik/HorribleSteve.java
@@ -1,3 +1,5 @@
+import org.junit.Test;
+
 public class HorribleSteve {
     public static void main(String [] args) throws Exception {
         int i = 0;
--- lab3/IntList/IntListTest.java
+++ lab3/IntList/IntListTest.java
@@ -66,6 +66,18 @@ public class IntListTest {
         assertEquals(IntList.of(1, 2, 3), A);
     }
 
+    @Test
+    public void testReverse() {
+        IntList A = IntList.of(1, 2, 3);
+        IntList B = IntList.of(4, 5, 6);
+        IntList expA = IntList.of(3, 2, 1);
+        IntList expB = IntList.of(4, 5, 6);
+        assertEquals(expA, IntList.reverse(A));
+        assertEquals(expB, IntList.reverse(B));
+        assertNotEquals(null, IntList.reverse(A));
+        assertEquals(null, IntList.reverse(A));
+    }
+
     /** If you're running this from the command line, you'll need
       * to add a main method. See ArithmeticTest.java for an
       * example. */
