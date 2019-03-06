af3ce26b33298dbb0483df749bc8c93a38f0ab88
Seungri Kim <srkim@berkeley.edu>
2019-03-05 17:48:01 -0800

    first

--- hw2/hw2/Percolation.java
+++ hw2/hw2/Percolation.java
@@ -3,5 +3,120 @@ package hw2;
 import edu.princeton.cs.algs4.WeightedQuickUnionUF;
 
 public class Percolation {
+    private boolean[][] grid;
+    private int openSite;
+    private int top;
+    private int bottom;
+    private int gridSize;
+    private int gridLength;
+    private WeightedQuickUnionUF tracker;
+    private WeightedQuickUnionUF fullTracker;
+
+    // create N-by-N grid, with all sites initially blocked
+    public Percolation(int N) {
+        if (N <= 0) {
+            throw new IllegalArgumentException("Size of grid must be greater than 0 * 0");
+        }
+        gridLength = N;
+        gridSize = N * N + 2;
+        tracker = new WeightedQuickUnionUF(gridSize);
+        fullTracker = new WeightedQuickUnionUF(gridSize);
+        openSite = 0;
+        top = 0;
+        bottom = gridSize - 1;
+        grid = new boolean[N][N];
+    }
+
+    // open the site (row, col) if it is not open already
+    public void open(int row, int col) {
+        if (!isOpen(row, col)) {
+            grid[row][col] = true;
+            int index = xyTo1D(row, col);
+            if (index <= gridLength) {
+                tracker.union(top, index);
+                fullTracker.union(top, index);
+            } else if (index >= bottom - gridLength) {
+                tracker.union(bottom, index);
+            }
+            openSite += 1;
+            //need to connect check
+            unionHelper(row, col, row + 1, col);
+            unionHelper(row, col, row - 1, col);
+            unionHelper(row, col,row, col - 1);
+            unionHelper(row, col,row, col + 1);
+        }
+    }
+
+    // is the site (row, col) open?
+    public boolean isOpen(int row, int col) {
+        if (!isInGrid(row, col)) {
+            throw new IndexOutOfBoundsException("One of Input is Out of Bound");
+        }
+        return grid[row][col];
+    }
+
+    // is the site (row, col) full?
+    public boolean isFull(int row, int col) {
+        if (!isInGrid(row, col)) {
+            throw new IndexOutOfBoundsException("One of Input is Out of Bound");
+        }
+        int index = xyTo1D(row, col);
+        if (fullTracker.connected(top,index)) {
+            return true;
+        }
+        return false;
+    }
+
+    // number of open sites
+    public int numberOfOpenSites() {
+        return openSite;
+    }
+
+    // does the system percolate?
+    public boolean percolates() {
+        return tracker.connected(top, bottom);
+    }
+
+    private int xyTo1D(int r, int c) {
+        return r * gridLength + c + 1;
+    }
+
+    private void unionHelper(int oriRow, int oriCol, int nearRow, int nearCol) {
+        if (isInGrid(nearRow, nearCol) && isOpen(nearRow, nearCol)) {
+            int oriIndex = xyTo1D(oriRow, oriCol);
+            int nearIndex = xyTo1D(nearRow, nearCol);
+            tracker.union(oriIndex, nearIndex);
+            fullTracker.union(oriIndex, nearIndex);
+        }
+    }
+
+    private boolean isInGrid(int row, int col) {
+        if (row < 0 || row > gridLength - 1 || col < 0 || col > gridLength - 1) {
+            return false;
+        }
+        return true;
+    }
+
+
+    public static void main(String[] args) {
+        Percolation test = new Percolation(3);
+        System.out.println(test.isOpen(0, 0)); //false
+        test.open(0, 0);
+        System.out.println(test.isOpen(0, 0)); //true
+        /*test.open(0, 4);
+        test.open(4, 0);
+        test.open(4, 4);
+        test.open(1,1);*/
+        System.out.println(test.isFull(1, 1)); //false
+        System.out.println(test.isFull(0, 0)); //true
+        test.open(1, 1);
+        test.open(1, 0);
+        System.out.println(test.isOpen(1, 0)); //true
+        System.out.println(test.isFull(1, 0)); //true
+        System.out.println(test.percolates()); //false
+        test.open(2, 1);
+        System.out.println(test.percolates()); //true
+        test.open(2, 2);
+    }
 
 }
--- hw2/hw2/PercolationStats.java
+++ hw2/hw2/PercolationStats.java
@@ -1,5 +1,51 @@
 package hw2;
 
-public class PercolationStats {
+import edu.princeton.cs.introcs.StdRandom;
+import edu.princeton.cs.introcs.StdStats;
 
+public class PercolationStats {
+    int T;
+    int numberOfSite;
+    int[] threshold;
+
+    // perform T independent experiments on an N-by-N grid
+    public PercolationStats(int N, int T, PercolationFactory pf) {
+        if (N <= 0 || T <= 0) {
+            throw new IllegalArgumentException();
+        }
+
+        T = T;
+        numberOfSite = 0;
+        Percolation newPercolchecker = pf.make(N);
+        for (int i = 0; i < T; i++) {
+            while (!newPercolchecker.percolates()) {
+                int row = StdRandom.uniform(N);
+                int col = StdRandom.uniform(N);
+                newPercolchecker.open(N, N);
+                numberOfSite++;
+            }
+            threshold[i] = numberOfSite / N * N;
+        }
+    }
+
+    // sample mean of percolation threshold
+    public double mean() {
+        return StdStats.mean(threshold);
+    }
+
+    // sample standard deviation of percolation threshold
+    public double stddev()  {
+        return StdStats.stddev(threshold);
+    }
+
+    // low endpoint of 95% confidence interval
+    public double confidenceLow() {
+        return mean() - ((1.96 * stddev()) / Math.sqrt(T));
+    }
+
+    // high endpoint of 95% confidence interval
+    public double confidenceHigh() {
+        return mean() + ((1.96 * stddev()) / Math.sqrt(T));
+    }
 }
+
