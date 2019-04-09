fc40528c20b56aafa08c26e4cd302a41e94a7d54
Seungri Kim <srkim@berkeley.edu>
2019-04-09 00:55:32 -0700

    modify edgeto

--- hw4/bearmaps/hw4/AStarSolver.java
+++ hw4/bearmaps/hw4/AStarSolver.java
@@ -2,10 +2,10 @@ package bearmaps.hw4;
 
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 
 import bearmaps.proj2ab.ArrayHeapMinPQ;
-import bearmaps.proj2ab.DoubleMapPQ;
 import edu.princeton.cs.algs4.Stopwatch;
 
 public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
@@ -18,31 +18,34 @@ public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
     public AStarSolver(AStarGraph<Vertex> input, Vertex start, Vertex end, double timeout) {
         solution = new ArrayList<>();
         Vertex current = start;
-        solution.add(start);
+        //solution.add(end);
         Stopwatch sw = new Stopwatch();
         ArrayHeapMinPQ<Vertex> fringe = new ArrayHeapMinPQ<>();
         //DoubleMapPQ<Vertex> fringe = new DoubleMapPQ<>();
         HashMap<Vertex, Double> distTo = new HashMap();
+        HashMap<Vertex, Vertex> edgeTo = new HashMap();
+        double heuristic = input.estimatedDistanceToGoal(start, end);
+        fringe.add(start, heuristic);
         distTo.put(start, 0.0);
-        //double heuristic;
 
         while (sw.elapsedTime() < timeout) {
-            List<WeightedEdge<Vertex>> neighbor = input.neighbors(current);
-
-            for (WeightedEdge<Vertex> e : neighbor) {
-                if (neighbor.size() == 0) {
+            //List<WeightedEdge<Vertex>> neighbor = input.neighbors(current);
+            for (WeightedEdge<Vertex> e : input.neighbors(current)) {
+                if (fringe.equals(null)) {
                     outcome = SolverOutcome.UNSOLVABLE;
                     return;
                 }
-
-                double heuristic = input.estimatedDistanceToGoal(e.to(), end);// has problem?
-
+                heuristic = input.estimatedDistanceToGoal(e.to(), end);// has problem?
+                //System.out.println("vertex is " + e.to());
+                //System.out.println("heuristic is " + heuristic);
                 if (!distTo.containsKey(e.to())) {
                     distTo.put(e.to(), e.weight() + distTo.get(e.from()));
+                    edgeTo.put(e.to(), e.from());
                     fringe.add(e.to(), distTo.get(e.to()) + heuristic);
                 } else {
                     if (distTo.get(e.from()) + e.weight() < distTo.get(e.to())) {
                         distTo.put(e.to(), distTo.get(e.from()) + e.weight());
+                        edgeTo.put(e.to(), e.from());
                         if (fringe.contains(e.to())) {
                             fringe.changePriority(e.to(), distTo.get(e.to()) + heuristic);
                         } else {
@@ -50,18 +53,14 @@ public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
                         }
                     }
                 }
-                //System.out.println("vertex is " + e.to());
-                //System.out.println("heuristic is " + input.estimatedDistanceToGoal(e.to(), end));
             }
-            /*if (!solution.contains(fringe.getSmallest())) {
-                solution.add(fringe.getSmallest());
-            }*/
-            solution.add(fringe.getSmallest());
+            //solution.add(fringe.getSmallest());
             //System.out.println("smallest is " + fringe.getSmallest());
             //System.out.println("heuristic is " + input.estimatedDistanceToGoal(fringe.getSmallest(), end));
             current = fringe.getSmallest();
             numStatesExplored += 1;
             if (fringe.getSmallest().equals(end)) {
+                solution = solutionHelper(edgeTo, start, end);
                 solutionWeight = distTo.get(current);
                 outcome = SolverOutcome.SOLVED;
                 timeSpent = sw.elapsedTime();
@@ -86,6 +85,17 @@ public class AStarSolver<Vertex> implements ShortestPathsSolver<Vertex> {
     }
     public double explorationTime() {
         return timeSpent;
+    }
+
+    private List<Vertex> solutionHelper(HashMap edgeTo, Vertex start, Vertex end) {
+        LinkedList<Vertex> tempSolution = new LinkedList<>();
+        tempSolution.addFirst(end);
+        while (!edgeTo.get(end).equals(start)) {
+            tempSolution.addFirst((Vertex) edgeTo.get(end));
+            end = (Vertex) edgeTo.get(end);
+        }
+        tempSolution.addFirst(start);
+        return tempSolution;
     }
 }
 
